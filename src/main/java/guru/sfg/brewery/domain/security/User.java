8703110c8058ae85159f1c335bca1346b2bbf11c
2peaks <woosang@hotmail.com>
2021-09-04 11:28:50 -0700

    Scheduled task for unlocking user accounts

--- src/main/java/guru/sfg/brewery/domain/security/User.java
+++ src/main/java/guru/sfg/brewery/domain/security/User.java
@@ -2,12 +2,15 @@ package guru.sfg.brewery.domain.security;
 
 import guru.sfg.brewery.domain.Customer;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.CredentialsContainer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -88,6 +91,13 @@ public class User implements UserDetails, CredentialsContainer {
 
     @Builder.Default
     private Boolean enabled = true;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
 
     @Override
     public void eraseCredentials() {
